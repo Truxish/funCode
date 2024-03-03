328ca9ffa78c95ca5a3149bfd2271302cb229d58
call me truck <161306979+Truxish@users.noreply.github.com>
2024-03-03 10:23:27 +0530

    Add files via upload

---
+++ bachelor-code/1/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#finalRose").addEventListener("click", clicked)
+
+
+function clicked(){
+	document.querySelector("#claire").style.display = "none"
+	document.querySelector("#sharleen").style.display = "none"
+}
